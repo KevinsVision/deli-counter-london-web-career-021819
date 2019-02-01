4691710d8472f3f38c6632847baa4943940c965e
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-02-01 00:29:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,6 @@ def now_serving(katz_deli)
   if katz_deli.length > 1
   #puts "Currently serving #{katz_deli}."
     puts "Currently serving #{katz_deli.shift}."
-
 else
   puts "There is nobody waiting to be served!"
 end
