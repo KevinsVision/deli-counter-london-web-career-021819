ecc0bbb0c51a68a9049ebfe8020572b11dd47c76
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-02-01 00:25:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,7 @@ def now_serving(katz_deli)
   #katz_deli.shift
   line = "Currently serving"
   katz_deli.each do |item|
-    line -= katz_deli.shift
+    line += katz_deli.shift
     line += "#{item}"
   end
 else
